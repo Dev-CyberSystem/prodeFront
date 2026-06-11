52885a4e6c8701a0c9558cbe631e9da24fcf92f2
Diego Olmi <diegolmi@hotmail.com>
2026-06-11 14:22:49 -0300

    uri

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const URI = "https://prodeback-wvvp.onrender.com/api"
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: URI || '/api',
 });
 
 api.interceptors.request.use((config) => {
